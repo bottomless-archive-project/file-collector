366acf0278c4d94c6c25bc5c6ac667976dc8a8fd
Gyula Lakatos <laxika91@gmail.com>
2021-08-25 21:31:36 +0200

    Started to work on the new architecture.

---
+++ file-collector-master/src/main/java/com/github/collector/FileCollectorMasterApplication.java
@@ -0,0 +1,12 @@
+package com.github.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCollectorMasterApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(FileCollectorMasterApplication.class, args);
+    }
+}
