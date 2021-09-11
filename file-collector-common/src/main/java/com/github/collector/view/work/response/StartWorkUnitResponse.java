2740478b1bcdef43f74c0899b1292723e1e3a4ae
Gyula Lakatos <laxika91@gmail.com>
2021-09-11 12:51:02 +0200

    Fixed an error when starting work units.

--- file-collector-common/src/main/java/com/github/collector/view/work/response/StartWorkUnitResponse.java
+++ file-collector-common/src/main/java/com/github/collector/view/work/response/StartWorkUnitResponse.java
@@ -2,11 +2,13 @@ package com.github.collector.view.work.response;
 
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.UUID;
 
 @Value
 @Builder
+@Jacksonized
 public class StartWorkUnitResponse {
 
     UUID id;
