e64b2a04ddcf0380b8a6bcbc2ec50c53e79f5b90
Gyula Lakatos <laxika91@gmail.com>
2021-09-12 12:12:54 +0200

    Revert "More tests."
    This reverts commit 58d5dfc6ff20f2616013a2474039a456d29a1195.

--- file-collector-slave/src/main/java/com/github/collector/service/deduplication/SourceLocationDeduplicationClient.java
+++ file-collector-slave/src/main/java/com/github/collector/service/deduplication/SourceLocationDeduplicationClient.java
@@ -1,19 +1,20 @@
 package com.github.collector.service.deduplication;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.collector.configuration.MasterServerConfigurationProperties;
+import com.github.collector.view.location.request.DeduplicateDocumentLocationRequest;
 import com.github.collector.view.location.response.DeduplicateDocumentLocationResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
-import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Collections;
+import java.net.URI;
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -21,46 +22,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SourceLocationDeduplicationClient {
 
-    private final HttpClient httpClient;
-    private final ObjectMapper objectMapper;
-    private final SourceDeduplicationRequestFactory sourceDeduplicationRequestFactory;
+    private final WebClient webClient = WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(HttpClient.from(TcpClient.newConnection())))
+            .build();
+    private final MasterServerConfigurationProperties masterServerConfigurationProperties;
 
     public Flux<String> deduplicateSourceLocations(final List<String> sourceLocations) {
         log.info("Deduplicating {} urls.", sourceLocations.size());
 
-        return Mono.fromCallable(() -> {
-                    try {
-                        log.info("Deduplication {} urls.", sourceLocations.size());
+        final DeduplicateDocumentLocationRequest deduplicateDocumentLocationRequest =
+                DeduplicateDocumentLocationRequest.builder()
+                        .locations(sourceLocations)
+                        .build();
 
-                        final HttpRequest request = sourceDeduplicationRequestFactory.newSourceDeduplicationRequest(sourceLocations);
+        return webClient.post()
+                .uri(URI.create(masterServerConfigurationProperties.getMasterLocation() + "/document-location"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(deduplicateDocumentLocationRequest)
+                .retrieve()
+                .bodyToFlux(DeduplicateDocumentLocationResponse.class)
+                .timeout(Duration.ofSeconds(30))
+                //.retry()
+                .flatMap(deduplicateDocumentLocationResponse -> {
+                    log.info("From the sent urls {} only {} was unique.", sourceLocations.size(),
+                            deduplicateDocumentLocationResponse.getLocations().size());
 
-                        final HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-                        final DeduplicateDocumentLocationResponse deduplicateDocumentLocationResponse =
-                                deserializeResponse(response);
-
-                        log.info("From the sent urls {} was unique.", deduplicateDocumentLocationResponse.getLocations().size());
-
-                        return deduplicateDocumentLocationResponse.getLocations();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-
-                        return Collections.<String>emptyList();
-                    } catch (IOException e) {
-                        //TODO: Add some retry logic!
-
-                        throw new IllegalStateException("Failed to do the deduplication!", e);
-                    }
-                })
-                .flatMapMany(locations -> {
-                    log.info("From the sent urls {} only {} was unique.", sourceLocations.size(), locations.size());
-
-                    return Flux.fromIterable(locations);
+                    return Flux.fromIterable(deduplicateDocumentLocationRequest.getLocations());
                 });
-    }
-
-    private DeduplicateDocumentLocationResponse deserializeResponse(final HttpResponse<String> response)
-            throws JsonProcessingException {
-        return objectMapper.readValue(response.body(), DeduplicateDocumentLocationResponse.class);
     }
 }
