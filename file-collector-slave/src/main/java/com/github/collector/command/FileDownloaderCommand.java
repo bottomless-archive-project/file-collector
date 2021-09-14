04d2752709e7401d2ec0802e9e3d650e41a816f9
Gyula Lakatos <laxika91@gmail.com>
2021-09-14 20:26:43 +0200

    Testing a new solution. Pre-crawling the URLs.

--- file-collector-slave/src/main/java/com/github/collector/command/FileDownloaderCommand.java
+++ file-collector-slave/src/main/java/com/github/collector/command/FileDownloaderCommand.java
@@ -6,6 +6,7 @@ import com.github.collector.service.download.DownloadTargetConverter;
 import com.github.collector.service.download.DownloadTargetFinalizer;
 import com.github.collector.service.download.SourceDownloader;
 import com.github.collector.service.validator.DownloadTargetValidator;
+import com.github.collector.service.work.domain.WorkUnit;
 import com.github.collector.service.workunit.WorkUnitGenerator;
 import com.github.collector.service.workunit.WorkUnitManipulator;
 import com.github.collector.service.workunit.WorkUnitParser;
@@ -14,8 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -34,24 +36,27 @@ public class FileDownloaderCommand implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Hooks.onOperatorDebug();
+        while (true) {
+            final WorkUnit workUnit = workUnitManipulator.startWorkUnit();
 
-        Flux.generate(workUnitGenerator)
-                .flatMap(workUnit -> Mono.just(workUnit)
-                        .flatMapMany(workUnitParser::parseSourceLocations)
-                        .buffer(100)
-                        .flatMap(sourceLocationDeduplicationClient::deduplicateSourceLocations)
-                        .flatMap(downloadTargetConverter::convert)
-                        .flatMap(sourceDownloader::downloadToFile)
-                        .flatMap(downloadTargetValidator::validateFiles)
-                        .buffer(100)
-                        .flatMap(fileDeduplicator::deduplicateFiles)
-                        .flatMap(downloadTargetFinalizer::finalizeDownloadTargets)
-                        .doOnError(error -> log.error("Failed to catch an error on the work unit level!", error))
-                        .then(Mono.just(workUnit))
-                )
-                .doOnNext(workUnitManipulator::closeWorkUnit)
-                .doOnError(error -> log.error("Failed to catch an error on the command level!", error))
-                .subscribe();
+            final List<String> urls = Mono.just(workUnit)
+                    .flatMapMany(workUnitParser::parseSourceLocations)
+                    .buffer()
+                    .blockLast();
+
+            Flux.fromIterable(urls)
+                    .buffer(100)
+                    .flatMap(sourceLocationDeduplicationClient::deduplicateSourceLocations)
+                    .flatMap(downloadTargetConverter::convert)
+                    .flatMap(sourceDownloader::downloadToFile)
+                    .flatMap(downloadTargetValidator::validateFiles)
+                    .buffer(100)
+                    .flatMap(fileDeduplicator::deduplicateFiles)
+                    .flatMap(downloadTargetFinalizer::finalizeDownloadTargets)
+                    .doOnError(error -> log.error("Failed to catch an error on the work unit level!", error))
+                    .blockLast();
+
+            workUnitManipulator.closeWorkUnit(workUnit);
+        }
     }
 }
