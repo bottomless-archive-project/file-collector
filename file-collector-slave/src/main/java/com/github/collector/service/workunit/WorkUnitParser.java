6a35093b1f0fe4e1c1f0eb6eb957a1fe81def583
Gyula Lakatos <laxika91@gmail.com>
2021-09-15 07:55:10 +0200

    Changed the URL parsing logic.

--- file-collector-slave/src/main/java/com/github/collector/service/workunit/WorkUnitParser.java
+++ file-collector-slave/src/main/java/com/github/collector/service/workunit/WorkUnitParser.java
@@ -1,13 +1,19 @@
 package com.github.collector.service.workunit;
 
-import com.github.bottomlessarchive.warc.service.WarcRecordStreamFactory;
-import com.github.bottomlessarchive.warc.service.record.domain.WarcRecordType;
+import com.github.bottomlessarchive.warc.service.WarcFormatException;
+import com.github.bottomlessarchive.warc.service.WarcRecordIteratorFactory;
+import com.github.bottomlessarchive.warc.service.content.domain.WarcContentBlock;
+import com.github.bottomlessarchive.warc.service.record.domain.WarcRecord;
+import com.github.collector.service.domain.ParsingContext;
 import com.github.collector.service.download.SourceLocationValidation;
 import com.github.collector.service.work.domain.WorkUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
@@ -20,10 +26,42 @@ public class WorkUnitParser {
     private final SourceLocationValidation sourceLocationValidation;
 
     public List<String> parseSourceLocations(final WorkUnit workUnit) {
-        return WarcRecordStreamFactory.streamOf(workUnit.getLocation(), WarcRecordType.RESPONSE)
-                .map(parsingContextFactory::buildParsingContext)
-                .flatMap(sourceLocationParser::parseLocations)
-                .filter(sourceLocationValidation::shouldCrawlSource)
-                .toList();
+        try {
+            final Iterator<WarcRecord<WarcContentBlock>> warcRecordIterator =
+                    WarcRecordIteratorFactory.iteratorOf(workUnit.getLocation());
+
+            final ArrayList<String> resultUrlList = new ArrayList<>();
+
+            while (true) {
+                try {
+                    if (!warcRecordIterator.hasNext()) {
+                        break;
+                    }
+
+                    final WarcRecord<WarcContentBlock> warcRecord = warcRecordIterator.next();
+
+                    // Skipping everything not a response
+                    if (!warcRecord.isResponse()) {
+                        continue;
+                    }
+
+                    final ParsingContext parsingContext = parsingContextFactory.buildParsingContext(warcRecord);
+
+                    final List<String> urls = sourceLocationParser.parseLocations(parsingContext)
+                            .filter(sourceLocationValidation::shouldCrawlSource)
+                            .toList();
+
+                    resultUrlList.addAll(urls);
+                } catch (WarcFormatException e) {
+                    log.error("Unable to parse warc file: " + e.getMessage());
+                }
+            }
+
+            return resultUrlList;
+        } catch (final Exception e) {
+            log.error("Failed to crawl urls!", e);
+
+            return Collections.emptyList();
+        }
     }
 }
