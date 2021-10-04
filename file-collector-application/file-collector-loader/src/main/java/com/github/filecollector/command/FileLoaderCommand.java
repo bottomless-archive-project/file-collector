18e6f1d3da3c32bb828a2bd5f9ad9aae1c626e29
Gyula Lakatos <laxika91@gmail.com>
2021-10-04 22:08:03 +0200

    Working on the loader.

--- file-collector-application/file-collector-loader/src/main/java/com/github/filecollector/command/FileLoaderCommand.java
+++ file-collector-application/file-collector-loader/src/main/java/com/github/filecollector/command/FileLoaderCommand.java
@@ -1,13 +1,65 @@
 package com.github.filecollector.command;
 
+import com.github.filecollector.workunit.service.WorkUnitService;
+import com.github.filecollector.workunit.service.domain.WorkUnit;
+import com.github.filecollector.workunit.service.domain.WorkUnitStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class FileLoaderCommand implements CommandLineRunner {
 
-    @Override
-    public void run(String... args) {
+    private final WorkUnitService workUnitService;
 
+    @Override
+    public void run(String... args) throws Exception {
+        int processedLines = 0;
+
+        log.info("Started creating work units.");
+
+        try (BufferedReader out = new BufferedReader(new FileReader(""))) {
+            boolean hasNext = true;
+
+            final List<String> urls = new ArrayList<>(1000);
+            do {
+                final String urlLine = out.readLine();
+
+                processedLines++;
+
+                if (urlLine == null) {
+                    hasNext = false;
+
+                    continue;
+                }
+
+                urls.add(urlLine);
+
+                if (urls.size() == 1000) {
+                    log.info("Creating a new work unit. Processed url lines: {}.", processedLines);
+
+                    workUnitService.finishWorkUnit(
+                            WorkUnit.builder()
+                                    .id(UUID.randomUUID())
+                                    .locations(urls)
+                                    .status(WorkUnitStatus.CREATED)
+                                    .build()
+                    );
+
+                    urls.clear();
+                }
+            } while (hasNext);
+        }
+
+        log.info("Finished processing the url file. Total lines processed: {}.", processedLines);
     }
 }
